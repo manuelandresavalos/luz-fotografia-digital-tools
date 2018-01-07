d1be49c1dc0e90ef8e33f51cb674ec00bf39f0c3
Manuel Avalos <manuel.avalos@globant.com>
2018-01-06 23:34:50 -0300

    Adding Zip.py to crear_pedido

---
+++ crear_pedido/zip.py
@@ -0,0 +1,18 @@
+import os
+import zipfile
+
+def zipMydir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+
+def zipMyfile(file, ziph):
+	ziph.write(file)
+
+if __name__ == '__main__':
+    zipf = zipfile.ZipFile('Pedido.zip', 'w', zipfile.ZIP_DEFLATED)
+    zipMydir('CON_BORDE', zipf)
+    zipMydir('SIN_BORDE', zipf)
+    zipMyfile('Pedido.txt', zipf)
+    zipf.close()
