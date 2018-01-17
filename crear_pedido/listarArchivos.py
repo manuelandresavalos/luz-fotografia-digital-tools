f74466533022ccd9ee28a86d12c6626a3a7cc1ec
manuelandresavalos <manuelandresavalos@gmail.com>
2018-01-17 16:36:50 -0300

    Adding modifications on ListarArchivos.py

--- crear_pedido/listarArchivos.py
+++ crear_pedido/listarArchivos.py
@@ -10,6 +10,7 @@ precios = {
 	'13x18': 7.95,
 	'15x21': 9.3,
 	'20x30': 25,
+	'30x40': 60.25,
 	'30x45': 60.25,
 }
 
@@ -17,6 +18,7 @@ totalesPorMedidas = {
 	'13x18': 0,
 	'15x21': 0,
 	'20x30': 0,
+	'30x40': 0,
 	'30x45': 0,
 }
 
@@ -42,26 +44,30 @@ for root, dirs, files in os.walk('./'):
 				totalesPorMedidas['15x21'] += lengthNum
 			if root.find("20x30") >= 0:
 				totalesPorMedidas['20x30'] += lengthNum
+			if root.find("30x40") >= 0:
+				totalesPorMedidas['30x40'] += lengthNum
 			if root.find("30x45") >= 0:
 				totalesPorMedidas['30x45'] += lengthNum
 			
 			stringFinal += "\n" + cadena
 
 
-costoTotal = totalesPorMedidas['13x18'] * precios['13x18'] + totalesPorMedidas['15x21'] * precios['15x21'] + totalesPorMedidas['20x30'] * precios['20x30'] + totalesPorMedidas['30x45'] * precios['30x45']
-cantidadTotal = totalesPorMedidas['13x18'] + totalesPorMedidas['15x21'] + totalesPorMedidas['20x30'] + totalesPorMedidas['30x45']
+costoTotal = totalesPorMedidas['13x18'] * precios['13x18'] + totalesPorMedidas['15x21'] * precios['15x21'] + totalesPorMedidas['20x30'] * precios['20x30'] + totalesPorMedidas['30x40'] * precios['30x40'] + totalesPorMedidas['30x45'] * precios['30x45']
+cantidadTotal = totalesPorMedidas['13x18'] + totalesPorMedidas['15x21'] + totalesPorMedidas['20x30'] + totalesPorMedidas['30x40'] + totalesPorMedidas['30x45']
 
 stringFinal += "\n"
 stringFinal += "\nCantidades Totales:"
 stringFinal += "\n13x18 = " + str(totalesPorMedidas['13x18'])
 stringFinal += "\n15x21 = " + str(totalesPorMedidas['15x21'])
 stringFinal += "\n20x30 = " + str(totalesPorMedidas['20x30'])
+stringFinal += "\n30x40 = " + str(totalesPorMedidas['30x40'])
 stringFinal += "\n30x45 = " + str(totalesPorMedidas['30x45'])
 stringFinal += "\n"
 stringFinal += "\nCostos:"
 stringFinal += "\n13x18 = " + str(totalesPorMedidas['13x18']) + " por $" + str(precios['13x18']) + " = $" + str(totalesPorMedidas['13x18'] * precios['13x18'])
 stringFinal += "\n15x21 = " + str(totalesPorMedidas['15x21']) + " por $" + str(precios['15x21']) + " = $" + str(totalesPorMedidas['15x21'] * precios['15x21'])
 stringFinal += "\n20x30 = " + str(totalesPorMedidas['20x30']) + " por $" + str(precios['20x30']) + " = $" + str(totalesPorMedidas['20x30'] * precios['20x30'])
+stringFinal += "\n30x40 = " + str(totalesPorMedidas['30x40']) + " por $" + str(precios['30x40']) + " = $" + str(totalesPorMedidas['30x40'] * precios['30x40'])
 stringFinal += "\n30x45 = " + str(totalesPorMedidas['30x45']) + " por $" + str(precios['30x45']) + " = $" + str(totalesPorMedidas['30x45'] * precios['30x45'])
 stringFinal += "\n"
 stringFinal += "\nCosto Total: $"+ str(costoTotal)
